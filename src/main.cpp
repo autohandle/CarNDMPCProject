59ee3abceef35310530bd625898a20bf06be692a
david <david@autohandle.com>
2017-11-20 16:00:37 -0800

    homogeneous transform to car

--- src/main.cpp
+++ src/main.cpp
@@ -66,31 +66,268 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
-const Eigen::Affine2d transformMapToCar(const double theCarMapPositionX, const double theCarMapPositionY, const double theCarMapRotation) {
-  const Eigen::Affine2d translate(Eigen::Translation<double,2>(theCarMapPositionX,theCarMapPositionY));
-  const Eigen::Affine2d rotate(Eigen::Rotation2D<double>(3.733));
-  return translate*rotate;
+const Eigen::Affine3d rotate(double ax, double ay, double az) {
+  const Eigen::Affine3d rx = Eigen::Affine3d(Eigen::AngleAxisd(ax, Eigen::Vector3d(1, 0, 0)));
+  const Eigen::Affine3d ry = Eigen::Affine3d(Eigen::AngleAxisd(ay, Eigen::Vector3d(0, 1, 0)));
+  const Eigen::Affine3d rz = Eigen::Affine3d(Eigen::AngleAxisd(az, Eigen::Vector3d(0, 0, 1)));
+  return rz * ry * rx;
 }
 
-std::array<std::vector<double>*,2> transformMapToCar(const Eigen::Affine2d theTransformation, const vector<double> theMapPositionsX, const vector<double> theMapPositionsY) {
-  assert (theMapPositionsX.size()==theMapPositionsY.size());
-  vector<double> *transformedPositionsX = new vector<double>;
-  vector<double> *transformedPositionsY = new vector<double>;;
-  for (int p=0; p<theMapPositionsX.size(); p++) {
-    Eigen::Vector3d mapPosition(theMapPositionsX[p], theMapPositionsY[p]);
-    Eigen::Vector3d transformedPosition = theTransformation*mapPosition;
-    (*transformedPositionsX).push_back(transformedPosition[0]);
-    (*transformedPositionsY).push_back(transformedPosition[1]);
+const Eigen::Rotation2D<double> rotateZ(double az) {
+  //const Eigen::Affine3d rz = Eigen::Affine3d(Eigen::AngleAxisd(az, Eigen::Vector3d(0, 0, 1)));
+  Eigen::Rotation2D<double> rotate(az);
+  return rotate;
+}
+
+const Eigen::Affine3d rotate(double az) {
+  return rotate(0., 0., az);
+}
+/*
+const Eigen::Affine3d mapToCarTransformation(const double theCarMapPositionX, const double theCarMapPositionY, const double theCarMapRotation) {
+  cout << "theCarMapPositionX:" << theCarMapPositionX << ", theCarMapPositionY:" << theCarMapPositionY << ", theCarMapRotation:" << theCarMapRotation << std::endl;
+  const Eigen::Affine3d translate(Eigen::Translation<double,3>(-theCarMapPositionX,-theCarMapPositionY, 1.));
+  const Eigen::Affine3d rotation=rotate(theCarMapRotation);
+  cout << "translate:" << std::endl << translate.matrix() << " ," << std::endl << "rotation:" << std::endl << rotation.matrix() << std::endl << " ," << std::endl << "rotate*translate:" << std::endl << (rotation*translate).matrix() << std::endl;
+  throw 221;
+  return rotation*translate;
+}
+*/
+const Eigen::Affine2d mapToCarTransformation(const double theCarMapPositionX, const double theCarMapPositionY, const double theCarMapRotation) {
+  cout << "theCarMapPositionX:" << theCarMapPositionX << ", theCarMapPositionY:" << theCarMapPositionY << ", theCarMapRotation:" << theCarMapRotation << std::endl;
+  const Eigen::Affine2d translation(Eigen::Translation<double,2>(-theCarMapPositionX,-theCarMapPositionY));
+  const Eigen::Affine2d  rotation((Eigen::Rotation2D<double>(theCarMapRotation)));
+  cout << "translation:" << std::endl << translation.matrix() << " ," << std::endl << "rotation:" << std::endl << rotation.matrix() << std::endl << " ," << std::endl << "rotation*translation:" << std::endl << (rotation*translation).matrix() << std::endl;
+  return rotation*translation;
+}
+
+const Eigen::Affine2d mapToCarTransformation(const Eigen::Vector2d theCarMapPosition, const double theCarMapRotation) {
+  return mapToCarTransformation(theCarMapPosition[0], theCarMapPosition[1], theCarMapRotation);
+}
+
+/*
+const Eigen::Affine3d mapToCarTransformation(const Eigen::Vector2d theCarMapPosition, const double theCarMapRotation) {
+  return mapToCarTransformation(theCarMapPosition[0], theCarMapPosition[1], theCarMapRotation);
+}
+*/
+const Eigen::Vector3d transformMapToCar(const Eigen::Affine2d theTransformationFromMapToCar, const Eigen::Vector3d theMapPosition) {
+  return theTransformationFromMapToCar*theMapPosition;
+}
+
+const Eigen::Vector2d transformMapToCar(const Eigen::Affine2d theTransformationFromMapToCar, const Eigen::Vector2d theMapPosition) {
+  const Eigen::Vector3d homogeneousPosition(theMapPosition[0], theMapPosition[1], 1.);
+  const Eigen::Vector3d transformedPosition=transformMapToCar(theTransformationFromMapToCar,homogeneousPosition);
+  const Eigen::Vector2d xyPosition(transformedPosition[0], transformedPosition[1]);
+  return xyPosition;
+}
+
+const vector<Eigen::Vector2d> transformMapToCar(const Eigen::Affine2d theTransformationFromMapToCar, const vector<Eigen::Vector2d> theMapPositions) {
+  vector<Eigen::Vector2d> transformedPositions;
+  for (int p=0; p<theMapPositions.size(); p++) {
+    transformedPositions.push_back(transformMapToCar(theTransformationFromMapToCar, theMapPositions[p]));
   }
-  std::array<std::vector<double>*,2> transformedPositions;
-  transformedPositions[0]=transformedPositionsX;
-  transformedPositions[1]=transformedPositionsY;
   return transformedPositions;
 }
 
+bool areSame(const double a, const double b, const double epsilon) {
+  return fabs(a - b) < epsilon;
+}
+
+bool isZero(const double a, const double epsilon) {
+  return fabs(a) < epsilon;
+}
+
+bool isNotZero(const double a, const double epsilon) {
+  return !isZero(a, epsilon);
+}
+
+bool areSame(const  Eigen::VectorXd a, const  Eigen::VectorXd b, const double epsilon) {
+  for (int r=0; r<a.rows(); r++) {
+    if (!areSame(a(r),b(r), epsilon)) {
+      std::cout << std::endl
+      << "a(" << r << "):" << a(r) << " != "
+      << "b(" << r << "):" << b(r) << " == "
+      << "fabs(a-b):" << fabs(a(r) - b(r))
+      << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+bool areSame(const  Eigen::MatrixXd a, const  Eigen::MatrixXd b, const double epsilon) {
+  for (int r=0; r<a.rows(); r++) {
+    for (int c=0; c<a.cols(); c++) {
+      if (!areSame(a(r,c),b(r,c), epsilon)) {
+        std::cout << std::endl
+        << "a(" << r << "," << c << "):" << a(r,c) << " != "
+        << "b(" << r << "," << c << "):" << b(r,c) << " == "
+        << "fabs(a-b):" << fabs(a(r,c) - b(r,c))
+        << std::endl;
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+static const double EPSILON=1.e-5;
+
+bool areSame(const  Eigen::VectorXd a, const  Eigen::VectorXd b) {
+  return areSame(a, b, EPSILON);
+}
+
+bool areSame( Eigen::MatrixXd a,  Eigen::MatrixXd b) {
+  return areSame(a, b, EPSILON);
+}
+
+bool areSame(double a, double b) {
+  return areSame(a, b, EPSILON);
+}
+
+bool isZero(double a) {
+  return isZero(a, EPSILON);
+  
+}
+
+bool isNotZero(double a) {
+  return isNotZero(a, EPSILON);
+}
+
+const int testCarSameAsMap() {
+  const Eigen::Vector2d carLocationOnMap(0.,0.);
+  const double carRotation=0.;
+  cout << "testCarCoordinatesSameAsMap-carLocationOnMap:" << carLocationOnMap.matrix() << std::endl << "mapToCarTransformation:" << mapToCarTransformation(carLocationOnMap, carRotation).matrix() << std::endl;
+  const Eigen::Vector2d mapLocation0(0.,0.);
+  const Eigen::Vector2d mapLocationFromCar0=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation0);
+  assert(areSame(mapLocationFromCar0[0],0.) && areSame(mapLocationFromCar0[1],0.));
+  const Eigen::Vector2d mapLocation1(1.,1.);
+  const Eigen::Vector2d mapLocationFromCar1=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation1);
+  cout << "testCarCoordinatesSameAsMap-mapLocationFromCar1:" << mapLocationFromCar1.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar1[0],1.) && areSame(mapLocationFromCar1[1],1.));
+  const Eigen::Vector2d mapLocation2(-1.,1.);
+  const Eigen::Vector2d mapLocationFromCar2=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation2);
+  assert(areSame(mapLocationFromCar2[0],-1.) && areSame(mapLocationFromCar2[1],1.));
+  const Eigen::Vector2d mapLocation3(-1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar3=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation3);
+  assert(areSame(mapLocationFromCar3[0],-1.) && areSame(mapLocationFromCar3[1],-1.));
+  const Eigen::Vector2d mapLocation4(1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar4=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation4);
+  assert(areSame(mapLocationFromCar4[0],1.) && areSame(mapLocationFromCar4[1],-1.));
+  return 0;
+}
+
+const int rotateCarAtOriginByPiOver2() {
+  const Eigen::Vector2d carLocationOnMap(0.,0.);
+  const double carRotation=-pi()/2.;
+  cout << "rotateCarCoordinatesAtOriginByPiOver2-carLocationOnMap:" << carLocationOnMap.matrix() << std::endl << "mapToCarTransformation:" << mapToCarTransformation(carLocationOnMap, carRotation).matrix() << std::endl;
+  const Eigen::Vector2d mapLocation0(0.,0.);// should stay same
+  const Eigen::Vector2d mapLocationFromCar0=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation0);
+  assert(areSame(mapLocationFromCar0[0],0.) && areSame(mapLocationFromCar0[1],0.));
+  const Eigen::Vector2d mapLocation1(1.,1.);
+  const Eigen::Vector2d mapLocationFromCar1=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation1);
+  cout << "rotateCarCoordinatesAtOriginByPiOver2-mapLocationFromCar1:" << mapLocationFromCar1.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar1[0],1.) && areSame(mapLocationFromCar1[1],-1.));
+  const Eigen::Vector2d mapLocation2(-1.,1.);
+  const Eigen::Vector2d mapLocationFromCar2=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation2);
+  assert(areSame(mapLocationFromCar2[0],1.) && areSame(mapLocationFromCar2[1],1.));
+  const Eigen::Vector2d mapLocation3(-1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar3=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation3);
+  assert(areSame(mapLocationFromCar3[0],-1.) && (mapLocationFromCar3[1],1.));
+  const Eigen::Vector2d mapLocation4(1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar4=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation4);
+  assert(areSame(mapLocationFromCar4[0],-1.) && areSame(mapLocationFromCar4[1],-1.));
+  return 0;
+}
+
+const int rotateCarAtOriginByPi() {
+  const Eigen::Vector2d carLocationOnMap(0.,0.);
+  const double carRotation=-pi();
+  cout << "rotateCarCoordinatesAtOriginByPiOver2-carLocationOnMap:" << carLocationOnMap.matrix() << std::endl << "transformMapToCar:" << mapToCarTransformation(carLocationOnMap, carRotation).matrix() << std::endl;
+  const Eigen::Vector2d mapLocation0(0.,0.);// should stay same
+  const Eigen::Vector2d mapLocationFromCar0=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation0);
+  assert(areSame(mapLocationFromCar0[0],0.) && areSame(mapLocationFromCar0[1],0.));
+  const Eigen::Vector2d mapLocation1(1.,1.);
+  const Eigen::Vector2d mapLocationFromCar1=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation1);
+  cout << "rotateCarCoordinatesAtOriginByPiOver2-mapLocationFromCar1:" << mapLocationFromCar1.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar1[0],-1.) && areSame(mapLocationFromCar1[1],-1.));
+  const Eigen::Vector2d mapLocation2(-1.,1.);
+  const Eigen::Vector2d mapLocationFromCar2=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation2);
+  assert(areSame(mapLocationFromCar2[0],1.) && areSame(mapLocationFromCar2[1],-1.));
+  const Eigen::Vector2d mapLocation3(-1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar3=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation3);
+  assert(areSame(mapLocationFromCar3[0],1.) && (mapLocationFromCar3[1],1.));
+  const Eigen::Vector2d mapLocation4(1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar4=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation4);
+  assert(areSame(mapLocationFromCar4[0],-1.) && areSame(mapLocationFromCar4[1],1.));
+  return 0;
+}
+
+const int translateCarToOneOne() {
+  const Eigen::Vector2d carLocationOnMap(1.,1.);
+  const double carRotation=0.;
+  cout << "translateCarCoordinatesToOneOne-carLocationOnMap:" << carLocationOnMap.matrix() << std::endl << "transformMapToCar:" << mapToCarTransformation(carLocationOnMap, carRotation).matrix() << std::endl;
+  const Eigen::Vector2d mapLocation0(0.,0.);// should stay same
+  const Eigen::Vector2d mapLocationFromCar0=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation0);
+  cout << "translateCarCoordinatesToOneOne-mapLocationFromCar0:" << mapLocationFromCar0.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar0[0],-1.) && areSame(mapLocationFromCar0[1],-1.));
+  const Eigen::Vector2d mapLocation1(1.,1.);
+  const Eigen::Vector2d mapLocationFromCar1=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation1);
+  cout << "translateCarCoordinatesToOneOne-mapLocationFromCar1:" << mapLocationFromCar1.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar1[0],0.) && areSame(mapLocationFromCar1[1],0.));
+  const Eigen::Vector2d mapLocation2(-1.,1.);
+  const Eigen::Vector2d mapLocationFromCar2=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation2);
+  assert(areSame(mapLocationFromCar2[0],-2.) && areSame(mapLocationFromCar2[1],0.));
+  const Eigen::Vector2d mapLocation3(-1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar3=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation3);
+  assert(areSame(mapLocationFromCar3[0],-2.) && (mapLocationFromCar3[1],-2.));
+  const Eigen::Vector2d mapLocation4(1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar4=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation4);
+  assert(areSame(mapLocationFromCar4[0],0.) && areSame(mapLocationFromCar4[1],-2.));
+  return 0;
+}
+
+const int translateCarToOneOneRotatePi() {
+  const Eigen::Vector2d carLocationOnMap(1.,1.);
+  const double carRotation=pi();
+  cout << "translateCarToOneOneRotatePi-carLocationOnMap:" << carLocationOnMap.matrix() << std::endl << "transformMapToCar:" << mapToCarTransformation(carLocationOnMap, carRotation).matrix() << std::endl;
+  const Eigen::Vector2d mapLocation0(0.,0.);
+  const Eigen::Vector2d mapLocationFromCar0=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation0);
+  cout << "translateCarToOneOneRotatePi-mapLocationFromCar0:" << mapLocationFromCar0.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar0[0],1.) && areSame(mapLocationFromCar0[1],1.));
+  const Eigen::Vector2d mapLocation1(1.,1.);
+  const Eigen::Vector2d mapLocationFromCar1=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation1);
+  cout << "translateCarToOneOneRotatePi-mapLocationFromCar1:" << mapLocationFromCar1.matrix() << std::endl;
+  assert(areSame(mapLocationFromCar1[0],0.) && areSame(mapLocationFromCar1[1],0.));
+  const Eigen::Vector2d mapLocation2(-1.,1.);
+  const Eigen::Vector2d mapLocationFromCar2=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation2);
+  assert(areSame(mapLocationFromCar2[0],2.) && areSame(mapLocationFromCar2[1],0.));
+  const Eigen::Vector2d mapLocation3(-1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar3=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation3);
+  assert(areSame(mapLocationFromCar3[0],2.) && (mapLocationFromCar3[1],2.));
+  const Eigen::Vector2d mapLocation4(1.,-1.);
+  const Eigen::Vector2d mapLocationFromCar4=transformMapToCar(mapToCarTransformation(carLocationOnMap, carRotation), mapLocation4);
+  assert(areSame(mapLocationFromCar4[0],0.) && areSame(mapLocationFromCar4[1],2.));
+  return 0;
+}
+
+const int testMappingToCar() {
+  testCarSameAsMap();// simplest car coordinates same as map
+  rotateCarAtOriginByPiOver2();// rotate to the north
+  rotateCarAtOriginByPi();// rotate to the west
+  translateCarToOneOne();
+  translateCarToOneOneRotatePi();
+  return 0;
+}
+
+const bool RUNTESTS = true;
+
 int main() {
   
-  std::cout << "mapToCarTransform:" << transformMapToCar(-40., 108., 3.733).matrix() << std::endl;
+  if (RUNTESTS) {
+    testMappingToCar();
+    return 0;
+  }
+  std::cout << "mapToCarTransform:" << mapToCarTransformation(-40., 108., 3.733).matrix() << std::endl;
   
   uWS::Hub h;
 
@@ -217,3 +454,5 @@ int main() {
   }
   h.run();
 }
+
+
